1d3b556768dd2562f43e4a627f58d61b255b7dfe
kelium_net_core@hotmail.com <kelium_net_core@hotmail.com>
2019-03-18 01:06:55 -0500

    Creacion del proyecto2

---
+++ Hellow_world2/Hellow_world2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Hellow_world2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
